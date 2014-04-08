0498a1eda4a34c6291b846384549e1c18b77203b
Liu Qian <332684980@qq.com>
2014-04-08 10:28:27 +0800

    Add key negotiation address

--- src/com/twlkyao/utils/ConstantVariables.java
+++ src/com/twlkyao/utils/ConstantVariables.java
@@ -20,6 +20,7 @@ public class ConstantVariables {
 	final public static String CHECK_FILE_INFO_URL = "cloud/android/check_data_info.php"; // The locaion of the file information PHP script
 	final public static String RETRIEVE_ENCRYPT_KEY = "cloud/android/keys_generator.php"; // The location of the keys generator PHP script.
 	final public static String CHECK_APK_INFO = "cloud/check_apk_info"; // The location of the check apk info PHP script.
+	final public static String NEGOTIATION_URL = "cloud/android/key_negotiation.php";
 	
 	// The type of message.
 	final String SMS_URI_ALL = "content://sms/"; // All sms.
